7caeaac864ba7ffd6b95df40197e6b2259e5bceb
Leopoldo Rodriguez Castelli <lrcastelli@icloud.com>
2022-08-26 12:13:23 -0300

    The Liskov Substitution.

---
+++ LiskovSubstitution.swift
@@ -0,0 +1,23 @@
+// Objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program
+// In other words, if you replace one object with another that's a subclass and this replacement could break the affected part.
+
+// ------------------------------------------------
+
+class Feline {
+    func makeNoise() {
+        print("Meow")
+    }
+}
+
+class Lion: Feline {
+    override func makeNoise() {
+        print("ROARR")
+    }
+}
+
+// This is where it violates the Liskov Substitution as doing so will break the parent class.
+class Cheetah: Feline {
+        override func makeNoise() {
+        fatalError("dunno what my sound is")
+    }
+}
